b8d1baaff3751bf8219bdf8aa4e823d704fbf1e8
Andreas Rossa <andreas@rossamail.de>
2025-02-23 22:39:44 +0100

    Add wii console type

--- lib/slippi/wii_console.ex
+++ lib/slippi/wii_console.ex
@@ -1,4 +1,10 @@
 defmodule Slippi.WiiConsole do
   @enforce_keys [:mac, :nickname, :ip]
   defstruct [:mac, :nickname, :ip]
+
+  @type t :: %__MODULE__{
+          mac: String.t(),
+          nickname: String.t(),
+          ip: String.t()
+        }
 end
