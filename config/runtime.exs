7ec4548f051b23c32fa2e11ba1a5deed214548a4
Andreas Rossa <andreas@rossamail.de>
2025-07-12 18:58:58 +0200

    add sentry config

--- config/runtime.exs
+++ config/runtime.exs
@@ -11,3 +11,10 @@ config :collector,
   replay_directory: System.get_env("REPLAY_DIRECTORY"),
   ws_ingestor_url: System.get_env("WS_INGESTOR_URL"),
   vercel_bypass_token: System.get_env("VERCEL_BYPASS_TOKEN")
+
+config :sentry,
+  dsn:
+    "https://a0d7c9f8d0b1bc69c36cba7ce37f6fa1@o4509623725850624.ingest.de.sentry.io/4509623735025744",
+  environment_name: Mix.env(),
+  enable_source_code_context: true,
+  root_source_code_paths: [File.cwd!()]
--- lib/collector/application.ex
+++ lib/collector/application.ex
@@ -3,6 +3,18 @@ defmodule Collector.Application do
 
   @spec start(any(), any()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start(_type, _args) do
+    :logger.add_handler(:sentry, Sentry.LoggerHandler, %{
+      config: %{metadata: [:file, :line]}
+    })
+
+    try do
+      a = 1 / 0
+      IO.puts(a)
+    rescue
+      my_exception ->
+        Sentry.capture_exception(my_exception, stacktrace: __STACKTRACE__)
+    end
+
     Collector.Supervisor.start_link(name: Collector.Supervisor)
   end
 end
